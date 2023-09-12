10471295aaefb4a1700cf232f717e3353085be29
kidmpukane <lwandisozwane91@gmail.com>
2023-09-12 06:02:08 +0200

    fix-fetch

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('user_profiles.urls')),
-    path('api/store/', include('store.urls'))
+    path('api/', include('store.urls'))
 
 ]
