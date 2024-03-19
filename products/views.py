88c660bb106522662317a7e22597d839af901ba5
kidmpukane <lwandisozwane91@gmail.com>
2024-03-19 14:02:29 +0200

    pre-algorithm-implementation

--- products/views.py
+++ products/views.py
@@ -4,13 +4,13 @@ from keras.datasets import fashion_mnist
 from rest_framework.response import Response
 from .models import (
     PrimaryProduct,
-    SecondaryProduct, 
+    SecondaryProduct,
     TertiaryProduct
 )
 from .serializers import (
     AllProductsSerializer,
-    PrimaryProductSerializer, 
-    SecondaryProductSerializer, 
+    PrimaryProductSerializer,
+    SecondaryProductSerializer,
     TertiaryProductSerializer,
     ProductInputSerializer
 )
@@ -21,20 +21,22 @@ from sklearn.preprocessing import StandardScaler
 import schedule
 import time
 
-#.......................................GET ALL USER DATA.......................................
+# .......................................GET ALL PRODUCTS.......................................
+
 
 @api_view(['GET'])
 def get_all_products(request):
     primary_products = PrimaryProduct.objects.all()
     secondary_products = SecondaryProduct.objects.all()
-    
+
     primary_serializer = PrimaryProductSerializer(primary_products, many=True)
-    secondary_serializer = SecondaryProductSerializer(secondary_products, many=True)
-    
+    secondary_serializer = SecondaryProductSerializer(
+        secondary_products, many=True)
+
     all_products = {
         "all_products": primary_serializer.data + secondary_serializer.data
     }
-    
+
     return Response(all_products)
 
 
@@ -42,6 +44,7 @@ def get_data(queryset, serializer_class):
     data = queryset.objects.all()
     serializer = serializer_class(data, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def get_all_primary_products(request):
@@ -58,7 +61,7 @@ def get_all_tertiary_products(request):
     return get_data(TertiaryProduct, TertiaryProductSerializer)
 
 
-#.......................................CREATE PRODUCT.......................................
+# .......................................CREATE PRODUCT.......................................
 
 def product_registration(serializer_class, request):
     serializer = serializer_class(data=request.data)
@@ -67,49 +70,57 @@ def product_registration(serializer_class, request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(['POST'])
 def create_primary_product(request):
     return product_registration(PrimaryProductSerializer, request)
+
 
 @api_view(['POST'])
 def create_secondary_product(request):
     return product_registration(SecondaryProductSerializer, request)
 
+
 @api_view(['POST'])
 def create_tertiary_product(request):
     return product_registration(TertiaryProductSerializer, request)
 
-#.......................................GET USER BY ID.......................................
+# .......................................GET PRODUCT BY ID.......................................
+
 
 def get_product(request, id, profile_model, serializer_class):
     try:
         user_profile_data = profile_model.objects.get(pk=id)
     except profile_model.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    
+
     if request.method == 'GET':
         serializer = serializer_class(user_profile_data)
         return Response(serializer.data)
+
 
 @api_view(['GET'])
 def get_primary_product(request, id):
     return get_product(request, id, PrimaryProduct, PrimaryProductSerializer)
 
+
 @api_view(['GET'])
 def get_secondary_product(request, id):
     return get_product(request, id, SecondaryProduct, SecondaryProductSerializer)
+
 
 @api_view(['GET'])
 def get_tertiary_product(request, id):
     return get_product(request, id, TertiaryProduct, TertiaryProductSerializer)
 
 
-#.......................................GET PRODUCT BY ID........................................
+# .......................................GET PRODUCT BY ID........................................
 
 @api_view(['GET'])
 def get_primary_product(request, primary_id):
     try:
-        primary_product_data, similar_primary_products = get_product_and_similars(primary_id, PrimaryProduct)
+        primary_product_data, similar_primary_products = get_product_and_similars(
+            primary_id, PrimaryProduct)
 
         response_data = {
             "primary_product": primary_product_data,
@@ -121,10 +132,12 @@ def get_primary_product(request, primary_id):
     except PrimaryProduct.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
+
 @api_view(['GET'])
 def get_secondary_product(request, secondary_id):
     try:
-        secondary_product_data, similar_secondary_products = get_product_and_similars(secondary_id, SecondaryProduct)
+        secondary_product_data, similar_secondary_products = get_product_and_similars(
+            secondary_id, SecondaryProduct)
 
         response_data = {
             "secondary_product": secondary_product_data,
@@ -135,7 +148,8 @@ def get_secondary_product(request, secondary_id):
 
     except SecondaryProduct.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-#.......................................EDIT STORE DATA.......................................
+# .......................................EDIT STORE DATA.......................................
+
 
 def update_data(request, id, queryset, serializer_class):
     user_profiles_info_update = queryset.objects.get(pk=id)
@@ -147,48 +161,57 @@ def update_data(request, id, queryset, serializer_class):
 
     return Response(serializer.data)
 
+
 @api_view(['PUT'])
 def edit_primary_product(request, id):
     return update_data(request, id, PrimaryProduct, PrimaryProductSerializer)
 
+
 @api_view(['PUT'])
 def edit_secondary_product(request, id):
     return update_data(request, id, SecondaryProduct, SecondaryProductSerializer)
+
 
 @api_view(['PUT'])
 def edit_tertiary_product(request, id):
     return update_data(request, id, TertiaryProduct, TertiaryProductSerializer)
 
 
-
-#.......................................DELETE STORE DATA.......................................
+# .......................................DELETE STORE DATA.......................................
 
 def delete_collection(request, id, queryset):
     delete_store_info = queryset.objects.get(pk=id)
     delete_store_info.delete()
     return Response("This store has been successfully deleted")
 
+
 @api_view(['DELETE'])
 def delete_primary_product(request, id):
     return delete_collection(request, id, PrimaryProduct)
 
+
 @api_view(['DELETE'])
 def delete_secondary_product(request, id):
     return delete_collection(request, id, SecondaryProduct)
+
 
 @api_view(['DELETE'])
 def delete_tertiary_product(request, id):
     return delete_collection(request, id, TertiaryProduct)
 
 
-#.......................................GET 5 NEAREST NEIGHBOR........................................
+# .......................................GET 5 NEAREST NEIGHBOR........................................
 
 
 def get_all_products_features():
-    primary_feature_lists = list(PrimaryProduct.objects.values_list('feature_list', flat=True))
-    secondary_feature_lists = list(SecondaryProduct.objects.values_list('feature_list', flat=True))
-    all_feature_lists = list(chain(primary_feature_lists, secondary_feature_lists))
+    primary_feature_lists = list(
+        PrimaryProduct.objects.values_list('feature_list', flat=True))
+    secondary_feature_lists = list(
+        SecondaryProduct.objects.values_list('feature_list', flat=True))
+    all_feature_lists = list(
+        chain(primary_feature_lists, secondary_feature_lists))
     return all_feature_lists
+
 
 def train_knn_model(features):
     scaler = StandardScaler()
@@ -196,8 +219,9 @@ def train_knn_model(features):
 
     knn = NearestNeighbors(n_neighbors=5)
     knn.fit(scaled_features)
-    
+
     return knn, scaler
+
 
 @api_view(['POST'])
 def find_the_nearest_neighbor(request):
@@ -217,7 +241,8 @@ def find_the_nearest_neighbor(request):
 
         similar_products_info = []
         for idx in similar_indices:
-            product = get_object_or_404(chain(PrimaryProduct.objects.all(), SecondaryProduct.objects.all()), pk=idx)
+            product = get_object_or_404(
+                chain(PrimaryProduct.objects.all(), SecondaryProduct.objects.all()), pk=idx)
             product_data = {
                 "id": product.id,
                 "product_name": product.product_name,
@@ -236,5 +261,5 @@ def find_the_nearest_neighbor(request):
 #     serializer = ProductInputSerializer(data=request.data)
 #     if serializer.is_valid():
 #         return Response(serializer.validated_data)
-    
+
 #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
